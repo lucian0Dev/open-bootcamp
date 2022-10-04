bf03704f8a74da05c5dddf3b76ec007d9d3cfc4a
luciano delgado <jlucianodelgado@gmail.com>
2022-10-04 17:30:49 -0300

    ejercicio 02

---
+++ javascriptBasico/ejercicio-02/index.js
@@ -0,0 +1,13 @@
+const lista = [
+    'luciano',
+    31,
+    true,
+    new Date (1991,6,13),
+    {
+        titulo: "El monje que vendió su Ferrari",
+        autor: "Robin S. Sharma",
+        fecha: new Date(1996, 0, 1),
+        url: "https://www.amazon.es/dp/B0062XBS32/"
+    },
+];
+console.log(lista)
