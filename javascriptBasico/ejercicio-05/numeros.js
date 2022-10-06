a92185bdfd30d7d3129d70d9292e451d885e6b89
luciano delgado <jlucianodelgado@gmail.com>
2022-10-06 16:44:07 -0300

    ejercicio 5 numeros

---
+++ javascriptBasico/ejercicio-05/numeros.js
@@ -0,0 +1,7 @@
+let alturaInt = 175;
+let alturaFloat = 1.75;
+let pesoFloat = 75.0;
+let alturaMetros = Math.ceil(alturaFloat);
+let pesoKilogramo = Math.floor(pesoFloat);
+const sonIguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE
+console.log(sonIguales)
