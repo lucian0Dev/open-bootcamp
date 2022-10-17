be54302c61d5210ac6cec4e24412ec190c373006
luciano delgado <jlucianodelgado@gmail.com>
2022-10-17 19:30:21 -0300

    conjuntos set js

---
+++ javascriptBasico/ejercicio-07/conjuntos.js
@@ -0,0 +1,6 @@
+const familiaArray = ["luciano","eliana","luis","rodrigo","ramiro","zoraida"];
+const familiaSet = new Set(familiaArray);
+familiaSet.add("luciano");
+console.log(familiaArray)
+familiaSet.add("javascript");
+console.log(familiaSet)
